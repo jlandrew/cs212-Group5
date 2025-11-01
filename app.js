10ad45ed2403679626c04c10617ad9ff4b03af2b
loganb7869 <lpb67@nau.edu>
2025-11-01 16:10:04 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+// Dark mode button
+const btn = document.getElementById('btn');
+
+// Dark mode event listener
+btn.addEventListener('click', function () {
+    document.body.classList.toggle('dark-mode');
+})
